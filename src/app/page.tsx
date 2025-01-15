8d6e32fc85c5385681015e489ae9774b05518056
N.I <iworksWeb@gmail.com>
2025-01-15 21:50:27 +0900

    Refactor Home page layout for improved user experience. Simplified structure by centering UserLocation component and removing unnecessary elements, enhancing clarity and responsiveness.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@ import { UserLocation } from "@/components/UserLocation";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <UserLocation />
-      </main>
+    <div className="flex justify-center items-center text-center p-8  font-[family-name:var(--font-geist-sans)]">
+      <UserLocation />
     </div>
   );
 }
